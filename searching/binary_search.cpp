0fda3b0920c27ed4c391d96208f94240ef8e217c
NujhatMaliha06 <malihanujhat99@gmail.com>
2026-05-31 15:59:54 +0600

    Add binary search solution

---
+++ searching/binary_search.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int binarySearch(vector<int> arr, int target) {
+    int left = 0;
+    int right = arr.size() - 1;
+
+    while (left <= right) {
+        int mid = (left + right) / 2;
+
+        if (arr[mid] == target) {
+            return mid;
+        }
+        else if (arr[mid] < target) {
+            left = mid + 1;
+        }
+        else {
+            right = mid - 1;
+        }
+    }
+
+    return -1;
+}
+
+int main() {
+    int n;
+    cout << "Enter number of elements: ";
+    cin >> n;
+
+    vector<int> arr(n);
+
+    cout << "Enter " << n << " sorted elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int target;
+    cout << "Enter target value: ";
+    cin >> target;
+
+    int ans = binarySearch(arr, target);
+
+    if (ans != -1) {
+        cout << "Target found at index: " << ans << endl;
+    }
+    else {
+        cout << "Target not found" << endl;
+    }
+
+    return 0;
+}
